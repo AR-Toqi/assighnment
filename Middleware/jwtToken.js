4671dbddcd2352b06d3241704b5a3da70c671dde
Toqi_Abdullah <abdullahragib1245@gmail.com>
2023-06-08 23:51:16 +0600

    assighnment commit

---
+++ Middleware/jwtToken.js
@@ -0,0 +1,32 @@
+// Task 3: JSON Web Tokens (JWT)
+
+
+const jwt = require('jsonwebtoken');
+const userId= "ab123";
+const secretKey= "toqi1234";
+
+function generateToken(userId, secretKey) {
+
+  const token = jwt.sign(userId, secretKey,);
+  return token;
+}
+generateToken(userId, secretKey);
+// Task 4: Express.js Middleware
+
+
+const authenticate= (req, res, next)=> {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, 'secretKey');
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+}
+module.exports=authenticate
---
+++ index.js
@@ -0,0 +1,22 @@
+// Task 2: Express.js Route
+
+
+
+
+const express = require('express');
+const Product = require('./model/productModel'); // Assuming the Product model is defined in a separate file
+
+const app = express();
+
+app.get('/products', async (req, res) => {
+  try {
+    const products = await Product.find({}, 'name price'); // Fetching only the name and price fields
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: 'An error occurred while retrieving the products.' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
